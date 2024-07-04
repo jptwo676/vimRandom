a4bdda2da150a6c20bb9b2f02f373f23acc35282
twoBelanson <twobakabaka656@gmail.com>
2024-07-03 22:41:28 -0300

    03/07/24

--- main.c
+++ main.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
 //abre um arquivo
 //mostra a data, dias consecutivos e uma quote random
 
-void detectaCAPS(char frase[]);
+int detectaHeader(char frase[]);
 
+//TODO reação pra quando a linha selecionada for vazia
+//TODO descobrir como pegar uma linha em especifico
 int main(){
-    int linhas = 0;
+
+    srand(time(NULL));
+    
+    //o arquivo tem 228 linhas
+    int linhaEscolhida = rand() % 228;
+    int linha = 0;
     char frase[300];
 
     FILE *fh_input;
     
     fh_input = fopen("kbindings.txt", "r");
-
     fgets(frase, 300, fh_input);
-    printf("%s", frase);
 
-    detectaCAPS(frase);
+    printf("linhaEscolhida = %d \n", linhaEscolhida);
+    if(detectaHeader(frase) == 0)
+    {
+        printf("%s", frase);
+    }
 
-    //TODO detectar se a senteça é caps
+    //TODO varrer por todas as linhas e parar no momento certo
+    for(int f = 0; f < 228; f++)
+    {
+        if(linhaEscolhida == f){
+            printf("linhaEscolhida = %d, f = %d \n", linhaEscolhida, f);
+
+    }
 
     fclose(fh_input); 
 
     return 0;
 }
 
-void detectaCAPS(char frase[])
+int detectaHeader(char frase[])
 {
     int fraseLen = 0;
+    int CAPS = 0;
 
     fraseLen = strlen(frase);
     printf("LEN = %d \n", fraseLen);
@@ -36,11 +54,13 @@ void detectaCAPS(char frase[])
     {
         if(frase[l] < 91 && frase[l] > 64)
         {
-            printf("Essa frase contem CAPS. \n");
+            CAPS++;
         }
-        else
+        if(CAPS > 4)
         {
-            printf("Essa frase não contem CAPS. \n");
+            printf("Esse é um Header. \n");
+            return 0;
         }
     }
+    return 1;
 }
